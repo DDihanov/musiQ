add97b7b40825dcbb81328db4812359ab13ef4cf
Unknown <dimitar.dihanov@gmail.com>
2017-09-14 10:34:04 +0300

    small changes with api config

---
+++ app/src/main/java/com/dihanov/musiq/config/Config.java
@@ -0,0 +1,11 @@
+package com.dihanov.musiq.config;
+
+import com.dihanov.musiq.BuildConfig;
+
+/**
+ * Created by Dimitar Dihanov on 14.9.2017 г..
+ */
+
+public class Config {
+    public static final String API_KEY = BuildConfig.API_KEY;
+}
